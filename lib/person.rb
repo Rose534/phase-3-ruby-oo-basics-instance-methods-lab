34c24ac7c20dc1207b19b60094af6d94b8db9cb8
rose534 <wambuirose534@gmail.com>
2023-02-10 17:26:23 +0300

    lab

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person 
+    def talk
+        puts "Hello World!"
+
+    def walk
+        puts "The Person is walking"
+    end 
+    end
+end
+
+fido = Person.new
+fido.talk
+
+fido = Person.new
+fido.walk
